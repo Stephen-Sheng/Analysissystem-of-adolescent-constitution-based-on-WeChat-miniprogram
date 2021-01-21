d31361dccdd0dacbf70f86f2afd5c04740eb3495
YutongSheng <544518449@qq.com>
2021-01-21 11:06:38 +0800

    update login function and fix bugs

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,8 @@
 
 Page({
   data: {
-    // userInfo: "test"
+    userInfo: wx.getStorageSync('userInfo'),
+    // Tabbar
     list: [{
       "text": "分析",
       "iconPath": "/images/AnalysisImg.png",
@@ -16,7 +17,11 @@ Page({
       // badge: 'New'
   }],
     showAnalysisDialog: false,
-    analysisDialogText: [{text: '确定'}]
+    analysisDialogText: [{text: '确定'}],
+    // 下拉菜单
+    selectShow: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ['15:10','15:15','15:20'],//下拉列表的数据
+    index: 0
   },
   //事件处理函数
   tabChange (e) {
@@ -26,8 +31,13 @@ Page({
     })
   },
   clickAnalysis: function() {
-    this.setData({
-      showAnalysisDialog:true
+      this.setData({
+        showAnalysisDialog:true
+      })
+  },
+  inClickAnalysis: function() {
+    wx.redirectTo({
+      url: '/pages/analysis/analysis',
     })
   },
   tapAnalysisDialogButton: function() {
@@ -35,19 +45,43 @@ Page({
       showAnalysisDialog: false
     })
   },
+  onLogin: function() {
+    wx.redirectTo({
+      url: '../userLogin/userLogin',
+    })
+  },
+  selectTap() {
+    this.setData({
+      selectShow: !this.data.selectShow
+    });
+  },
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+      index: Index,
+      selectShow: !this.data.selectShow
+    });
+  },
   onLaunch: function () {
-    console.log('Page1 launch');
+    
   },
-  onShow: function() {
-    console.log('Page1 Show');
-  },
+  // onShow: function() {
+  //   console.log('Page1 Show');
+  // },
   onReady: function() {
-    console.log('Page1 Ready');
+    var loginJud = wx.getStorageSync('loginJud');
+    var userStorage = wx.getStorageSync('userInfo');
+    if(userStorage && loginJud ){
+      this.setData({
+        userInfo: userStorage
+      })
+    }
   },
-  onHide: function() {
-    console.log('Page1 Hide');
-  },
+  // onHide: function() {
+  //   console.log('Page1 Hide');
+  // },
   onUnload: function() {
     console.log('Page1 Unload');
+    
   }
 })
--- pages/userLogin/userLogin.js
+++ pages/userLogin/userLogin.js
@@ -5,62 +5,100 @@ Page({
    * 页面的初始数据
    */
   data: {
+    phoneNum:'',
+    pwd:'',
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    loginInfo:'',
+    loginJud: false,
+    code:''
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  bindPhoneInput: function (e) {
+    var value = e.detail.value; //获取输入的内容
+    this.setData({
+      phoneNum:value,//改变page--data中phoneNum的值
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  bindPwdInput: function (e) {
+    var value = e.detail.value; //获取输入的内容
+    this.setData({
+      pwd:value,//改变page--data中phoneNum的值
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindGetUserInfo (e) {
+    console.log(e.detail.userInfo)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  submitAcc: function () {
+    var that = this;
+    if(that.data.phoneNum.length ==0 || that.data.pwd.length ==0){//校验非空
+      wx.showToast({  //弹框提示
+        icon: 'none',
+        title: '用户名或密码不能为空！',
+        duration: 2000,
+      })
+    }else {
+      wx.request({  //向后台发送请求
+        url: 'https://www.stephensheng.cn:8764/login',
+        method: "get",
+        header: { 'content-type': 'application/json' },
+        data: {
+          phoneNum: this.data.phoneNum, //this.data.phoneNum 代表你data中phoneNum的值
+          pwd: this.data.pwd,
+        },
+        success: function (res) { //res为后台返回给前端的数据
+          that.setData({
+            loginInfo: res.data.data.info,
+            code: res.data.code,
+               
+          })
+          if(that.data.code == 200){ //如果返回的code为200，代表用户名密码验证成功
+            wx.showToast({
+              title: '登录成功',
+            })
+            that.setData({
+              loginJud: true 
+            })
+            wx.setStorageSync('loginJud', that.data.loginJud); //保存userId至本地，以便随时调用
+            wx.getSetting({
+              success:(res) => {
+                // 判断用户权限
+                if (res.authSetting['scope.userInfo'] == undefined)
+                {
+                  // 如果未开启权限
+                  wx.authorize({
+                    scope: 'scope.userInfo',
+                    success:() => {
+                      // 用户点击确定后执行此处代码。跳转页面
+                      wx.redirectTo({
+                        url: '../index/index',
+                      })
+                    }
+                  })
+                }
+                // 若用户已开启权限，直接跳转页面。
+                else
+                {
+                  wx.getUserInfo({
+                    success: function(res){
+                      wx.setStorageSync('userInfo', res.userInfo);
+                      wx.redirectTo({
+                        url: '../index/index',
+                      })
+                    }
+                  });
+                }
+              }
+            })
+          }else{
+            wx.showToast({
+              icon: 'none',
+              title: '用户名或密码错误',
+            })
+          }
+ 
+        }
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
